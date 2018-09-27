4a870fadc12080e31b74ad10094b9234a5140d15
pauladrian45 <pauladrian45@gmail.com>
2018-09-26 22:23:07 -0500

    archivo

--- practica2.ino
+++ practica2.ino
@@ -1,33 +1,29 @@
 #include "LowPower.h"
 #define led_verde 4
-#define led_rojo 3
 #define sensor 2
 #define alarma 9
 int length = 15; // el numero de las notas
 char notes[] = "ccggaagffeeddc "; 
 int beats[] = { 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2, 4 };
-int tempo = 300;
+int tempo = 600;
 
 void setup() {
   pinMode(sensor, INPUT_PULLUP);//establecer el switch como entrada
   pinMode(led_verde, OUTPUT);//poner el led como salida
-  pinMode(led_rojo, OUTPUT);//poner el led como salida
   pinMode(alarma, OUTPUT);//poner la bocina como salida.
   Serial.begin(9600);
 }
 void loop() {
   //si el iman se acerca al reed switch
-  if(digitalRead(sensor)==LOW){
-          digitalWrite(led_rojo,LOW);//apaga el red rojo
-          sonido();//suena la alarma
-        }else {
-          //si el iman esta lejos del reed switch
-          attachInterrupt(0,sensor,FALLING);//Interrupcion para bajar el consumo de energia
-          LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);//Apaga ciertas funciones del Arduino para bajar el consumo de energia
-          detachInterrupt(0);
-          digitalWrite(led_rojo, HIGH);//prende el led rojo 
-    }
+  attachInterrupt(0,sistema,FALLING);//Interrupcion para bajar el consumo de energia
+  LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);//Apaga ciertas funciones del Arduino para bajar el consumo de energia
+  detachInterrupt(0);
+}
 
+
+
+void sistema(){
+  sonido();//proceso de alarma
 }
 
 void sonido(){
@@ -47,11 +43,11 @@ void sonido(){
 void playTone(int tone, int duration) {
   //metodo para calcular los tonos de la melodia de la bocina
   for (long i = 0; i < duration * 1000L; i += tone * 2) {
-    digitalWrite(led_verde, HIGH);//apaga el led
     digitalWrite(alarma, HIGH);
+    digitalWrite(led_verde, HIGH);//apaga el led
     delayMicroseconds(tone);
-    digitalWrite(led_verde, LOW);//enciende el led
     digitalWrite(alarma, LOW);
+    digitalWrite(led_verde, LOW);//enciende el led
     delayMicroseconds(tone);
   }
 }
